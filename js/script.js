67fead1a9eccede0d0274792ff43b1e7f0511a68
Brianna Florio <brianna.florio@stonybrook.edu>
2019-03-12 12:26:42 -0400

    Added blank HTML page, edited JS to display coordinates on screen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-document.onmousemove = function(e) {
-    var x = e.pageX;
-    var y = e.pageY;
+var coordinates = {};
+coordinates.string = "";
+coordinates.x = 0;
+coordinates.y = 0;
+
+document.onmousemove = function(e)
+{
+    coordinates.x = e.pageX;
+    coordinates.y = e.pageY;
+    coordinates.string = "(x coordinates: "+coordinates.x+", y coordinates: "+coordinates.y+")";
+    $("#coordinates").html(coordinates.string);
 };
